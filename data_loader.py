fd4847eeee01839c1a979322b3332062359fd0c4
RenoMG <reno@renos.world>
2026-01-28 19:17:13 -0800

    if directory not created, do nothing

--- data_loader.py
+++ data_loader.py
@@ -9,7 +9,9 @@ config_file = "config.json"
 
 # This is only used for after first_boot operations.
 try:
-    with open(p / config_file, "r") as f:
-        config_data = json.load(f)
+    if p.exists() != False:   
+
+        with open(p / config_file, "r") as f:
+            config_data = json.load(f)
 except Exception as e:
     print(f"Oops! I cannot load my system config! ERROR:{e}")
