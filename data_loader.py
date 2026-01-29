9fce2e3a5fb29e310fa0f3dd084309370abe97e6
RenoMG <reno@renos.world>
2026-01-28 18:43:51 -0800

    change data loader to use pathlib for OS compatability

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,13 @@
 import json
+from pathlib import Path
+
+# Load config location with pathlib for multi-os compatibility
+p = Path("storage/")
+config_file = "config.json"
 
 # This is only used for after first_boot operations.
 try:
-    with open("storage/config.json", "r") as f:
+    with open(p / config_file, "r") as f:
         config_data = json.load(f)
 except Exception as e:
     print(f"Oops! I cannot load my system config! ERROR:{e}")
